0e7b681fe8241ef413a1c1b2f790ba47a9a53824
d3rupt <thdnkns@gmail.com>
2020-12-08 08:59:04 -0600

    instructions overlay

--- views/script.js
+++ views/script.js
@@ -9,6 +9,8 @@ const myPeer = new Peer(undefined, {
 	secure: "true",
 	port: "443"
 });
+
+
 document.querySelector('.load').classList.remove('loading');
 window.onload = () => {
 	setTimeout(() => {
@@ -21,141 +23,93 @@ window.onload = () => {
 let videoSrc;
 let movieUser;
 let movietime;
+let clientsList;
 
 socket.on('movie-check', (movieUserServer, movieStateServer, clientsList) => {
 	movieUser = movieUserServer;
-  console.log('MOVIE CHECK');
+	console.log('MOVIE CHECK');
 	movieTime(movieStateServer)
 	movieState = movieStateServer;
-
-const myVideo = document.createElement('video')
-myVideo.muted = true
-	if (clientsList <= 1) {
-		videoSrc = 0;
-		socket.emit('movie');
-
-		navigator.mediaDevices.getDisplayMedia = navigator.mediaDevices.getDisplayMedia ||
-	navigator.mediaDevices.webkitGeDisplayMedia ||
-	navigator.mediaDevices.mozGetDisplayMedia || navigator.mediaDevices.msGetDisplayMedia;
-		navigator.mediaDevices.getDisplayMedia({
-			video: true,
-			audio: true
-		}).then(stream => {
-			addVideoStreamMovie(myVideo, stream)
-			popup.classList.add('popupClicked');
-			myPeer.on('call', call => {
-				console.log('STARTING STREAM')
-				call.answer(stream)
-				console.log('caught stream')
-				const video = document.createElement('video')
-				call.on('stream', userVideoStream => {
-					console.log('adding peep');
-					addVideoStreamPeep(video, userVideoStream);
-				})
-		})
-		socket.on('user-connected', userId => {
-			console.log('connecting ' + userId);
-			setTimeout(function ()
-				{
-					connectToNewUser(userId, stream);
-				},5000
-			)
-		})
-	}).catch(error());
-} else {
-	popup.addEventListener('click', (event) => {
-	     if (event.target.id == 'mic' || event.target.classList.contains('mic')) {
-	      videoSrc = 1;
-				navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia ||
-			navigator.mediaDevices.webkitGetUserMedia ||
-			navigator.mediaDevices.mozGetUserMedia || navigator.mediaDevices.msGetUserMedia;
-	      navigator.mediaDevices.getUserMedia({
-	        video: false,
-	        audio: true
-	      }).then(stream => {
-	        addVideoStreamPeep(myVideo, stream)
-					popup.classList.add('popupClicked');
-
-	        myPeer.on('call', call => {
-	          console.log('STARTING STREAM')
-	          call.answer(stream)
-	          console.log('CONNECTION ID: ' + call.peer)
-	          const video = document.createElement('video');
-	          call.on('stream', userVideoStream => {
-							let compare;
-	            console.log('STARTING STREAM')
-							console.log('CALL.PEER: ' + `'${call.peer}'`);
-							console.log('MOVIEUSER: ' + `'${movieUser}'`);
-							if (typeof(movieUser) != 'undefined') {
-							compare = movieUser.localeCompare(call.peer);
-							console.log('CALL = PEER?: ' + compare);
-						} else {
-							compare = 1;
-						}
-	            if (compare == 0) {
-								console.log('STARTING movie')
-	              addVideoStreamMovie(video, userVideoStream);
-	            } else {
-								console.log('STARTING peep')
-	              addVideoStreamPeep(video, userVideoStream);
-	            }
-	          })
-	      })
-	      socket.on('user-connected', userId => {
-					console.log('connecting ' + userId);
-
-	        setTimeout(function ()
-	          {
-	            connectToNewUser(userId, stream);
-	          },5000
-	        )
-	      })
-	    })
-	}     else if (event.target.id == 'noChat' || event.target.classList.contains('noChat')) {
-	      videoSrc = 1;
-	        addVideoStreamPeep(myVideo, undefined)
-					popup.classList.add('popupClicked');
-
-	        myPeer.on('call', call => {
-	          console.log('STARTING STREAM')
-	          call.answer()
-	          console.log('CONNECTION ID: ' + call.peer)
-	          const video = document.createElement('video');
-	          call.on('stream', userVideoStream => {
-							let compare;
-	            console.log('STARTING STREAM')
-							console.log('CALL.PEER: ' + `'${call.peer}'`);
-							console.log('MOVIEUSER: ' + `'${movieUser}'`);
-							if (typeof(movieUser) != 'undefined') {
-							compare = movieUser.localeCompare(call.peer);
-							console.log('CALL = PEER?: ' + compare);
-						} else {
-							compare = 1;
-						}
-	            if (compare == 0) {
-								console.log('STARTING movie')
-	              addVideoStreamMovie(video, userVideoStream);
-	            } else {
-								console.log('STARTING peep')
-	              addVideoStreamPeep(video, userVideoStream);
-	            }
-	          })
-	      })
-	      socket.on('user-connected', userId => {
-					console.log('connecting ' + userId);
-
-	        setTimeout(function ()
-	          {
-	            connectToNewUser(userId, stream);
-	          },5000
-	        )
-	      })
-
-	}
-	}, true)
-}
 })
 
+document.querySelector('.skipButton').addEventListener('click', () => {
+
+	const myVideo = document.createElement('video')
+	myVideo.muted = true
+		if (clientsList <= 1) {
+			videoSrc = 0;
+			socket.emit('movie');
+
+			navigator.mediaDevices.getDisplayMedia = navigator.mediaDevices.getDisplayMedia ||
+		navigator.mediaDevices.webkitGeDisplayMedia ||
+		navigator.mediaDevices.mozGetDisplayMedia || navigator.mediaDevices.msGetDisplayMedia;
+			navigator.mediaDevices.getDisplayMedia({
+				video: true,
+				audio: true
+			}).then(stream => {
+				addVideoStreamMovie(myVideo, stream)
+				popup.classList.add('popupClicked');
+				myPeer.on('call', call => {
+					call.answer(stream)
+					const video = document.createElement('video')
+					call.on('stream', userVideoStream => {
+						addVideoStreamPeep(video, userVideoStream);
+					})
+			})
+			socket.on('user-connected', userId => {
+				console.log('connecting ' + userId);
+				setTimeout(function ()
+					{
+						connectToNewUser(userId, stream);
+					},5000
+				)
+			})
+		}).catch(error());
+	} else {
+		popup.addEventListener('click', (event) => {
+		     if (event.target.id == 'mic' || event.target.classList.contains('mic')) {
+		      videoSrc = 1;
+					navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia ||
+				navigator.mediaDevices.webkitGetUserMedia ||
+				navigator.mediaDevices.mozGetUserMedia || navigator.mediaDevices.msGetUserMedia;
+		      navigator.mediaDevices.getUserMedia({
+		        video: false,
+		        audio: true
+		      }).then(stream => {
+		        addVideoStreamPeep(myVideo, stream)
+						popup.classList.add('popupClicked');
+
+		        myPeer.on('call', call => {
+		          call.answer(stream)
+		          const video = document.createElement('video');
+		          call.on('stream', userVideoStream => {
+								let compare;
+								if (typeof(movieUser) != 'undefined') {
+								compare = movieUser.localeCompare(call.peer);
+							} else {
+								compare = 1;
+							}
+		            if (compare == 0) {
+		              addVideoStreamMovie(video, userVideoStream);
+		            } else {
+		              addVideoStreamPeep(video, userVideoStream);
+		            }
+		          })
+		      })
+		      socket.on('user-connected', userId => {
+		        setTimeout(function ()
+		          {
+		            connectToNewUser(userId, stream);
+		          },5000
+		        )
+		      })
+		    })
+		}     else if (event.target.id == 'noChat' || event.target.classList.contains('noChat')) {
+			peepStart();
+		}
+		}, true)
+	}
+})
 
 socket.on('movie-times', movieStateServer => {
 	movieTime(movieStateServer);
@@ -235,6 +189,9 @@ function addVideoStreamPeep(video, stream) {
 	contextMenu.appendChild(slider);
 	peepContainer.appendChild(contextMenu);
 }
+document.querySelector('.skipButton').addEventListener('click', () => {
+	document.querySelector('.overlay').style.display = 'none';
+})
 
 peeps.addEventListener("click", () => {
 	if (event.target.classList.contains('peepVideo')) {
@@ -268,6 +225,8 @@ function movieTime(movieStateServer) {
 	document.querySelector('.tvEffect').classList.remove('tvBackgroundMovieTime');
 	document.querySelector('.cover').addEventListener('transitionend', () => {
 		document.querySelector('.mainWrapper').classList.remove('paddingOff');
+		document.querySelector('.mainWrapper').requestFullscreen()
+
 		//closeFullscreen();
 	})
 } else if (movieStateServer == 1) {
@@ -278,11 +237,11 @@ function movieTime(movieStateServer) {
 	document.querySelector('.tvEffect').classList.add('tvBackgroundMovieTime');
 	document.querySelector('.cover').addEventListener('transitionend', () => {
 		document.querySelector('.mainWrapper').classList.add('paddingOff');
+		document.exitFullscreen()
 		console.log('Blafdgfgf');
 		//openFullscreen();
- 	})
-
-}
+ 		})
+	}
 }
 /* View in fullscreen */
 function openFullscreen() {
@@ -329,6 +288,35 @@ function localMovieTime() {
 
 	})
 }}
+const nextButton1 = document.querySelector('.nextButton1');
+const nextButton2 = document.querySelector('.nextButton2');
+const finishButton = document.querySelector('.finishButton');
+const overlay = document.querySelector('.overlayCarousel');
+const oc1 = document.getElementById('oc1');
+const oc2 = document.getElementById('oc2');
+const oc3 = document.getElementById('oc3');
+
+/*overlay.addEventListener("click", (event) => {
+	if (event.target.classList.contains('nextButton1')) {
+	console.log('clicked');
+	oc1.classList.add('seethrough');
+	oc1.classList.remove('seeyou');
+	oc2.classList.add('seeyou');
+	oc1.ontransitionend = () => {
+		oc1.style.display = "none";
+	}
+} else if (event.target.classList.contains('nextButton2')) {
+	console.log('clicked2');
+	oc2.classList.add('seethrough');
+	oc2.classList.remove('seeyou');
+	oc3.classList.add('seeyou');
+	oc2.ontransitionend = () => {
+		oc2.style.display = "none";
+	}
+} else if (event.target.classList.contains('skipButton')) {
+
+}
+});*/
 
 document.querySelector('.movieButton').addEventListener("click", () => {
 	if (movieState == 0) {
@@ -355,3 +343,104 @@ let color;
 		}
 	}
 })*/
+
+function peepStart() {
+	videoSrc = 1;
+		addVideoStreamPeep(myVideo, undefined)
+		popup.classList.add('popupClicked');
+
+		myPeer.on('call', call => {
+			call.answer()
+			const video = document.createElement('video');
+			call.on('stream', userVideoStream => {
+				let compare;
+				if (typeof(movieUser) != 'undefined') {
+				compare = movieUser.localeCompare(call.peer);
+			} else {
+				compare = 1;
+			}
+				if (compare == 0) {
+					addVideoStreamMovie(video, userVideoStream);
+				} else {
+					addVideoStreamPeep(video, userVideoStream);
+				}
+			})
+	})
+	socket.on('user-connected', userId => {
+		console.log('connecting ' + userId);
+
+		setTimeout(function ()
+			{
+				connectToNewUser(userId, stream);
+			},5000
+		)
+	})
+}
+
+
+let slideIndex = 1;
+let slides = document.getElementsByClassName('oc');
+let inds = document.getElementsByClassName("overlayInd");
+inds[0].style.background = 'rgb(0,0,0,0.5)';
+document.querySelector('.prevButton').style.opacity = '0';
+document.querySelector('.prevButton').style.pointerEvents = 'none';
+function plusSlides(n) {
+  console.log('clicked');
+  showSlides(slideIndex += n)
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+function showSlides(n) {
+  let i;
+
+  if ( n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";
+  }
+	for ( i = 0; i < inds.length; i++) {
+    inds[i].style.backgroundColor = "rgba(0,0,0,0)";
+  }
+
+  slides[slideIndex-1].style.opacity = "1";
+  inds[slideIndex-1].style.backgroundColor = "rgba(0,0,0,0.5)";
+
+	if (slides[slideIndex-1].id == 'oc3') {
+		document.querySelector('.nextButton1').style.opacity = '0.5';
+		document.querySelector('.nextButton1').style.pointerEvents = 'none';
+		document.querySelector('.skipButtonText').textContent = 'Close'
+	} else {
+		document.querySelector('.nextButton1').style.opacity = '1';
+		document.querySelector('.nextButton1').style.pointerEvents = 'auto';
+		document.querySelector('.skipButtonText').textContent = 'Skip'
+	}
+	if (slides[slideIndex-1].id == 'oc1') {
+		document.querySelector('.prevButton').style.opacity = '0';
+		document.querySelector('.prevButton').style.pointerEvents = 'none';
+	} else {
+		document.querySelector('.prevButton').style.opacity = '1';
+		document.querySelector('.prevButton').style.pointerEvents = 'auto';
+
+	}
+}
+
+function copyLink() {
+	let dummy = document.createElement('input');
+	let link = window.location.href;
+
+	document.body.appendChild(dummy);
+
+	dummy.value = link;
+	dummy.select()
+	document.execCommand('copy');
+	document.body.removeChild(dummy);
+	document.querySelector('.fa-check').classList.remove('seethrough')
+	setTimeout(() => {
+		document.querySelector('.fa-check').classList.add('seethrough')
+	}, 750)
+}
